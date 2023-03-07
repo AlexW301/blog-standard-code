feb91198c527d2a449fc51172191b4ab96fa5992
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2023-03-07 08:59:41 -0500

    3/7/23

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['s.gravitar.com', 'lh3.googleusercontent.com']
+  }
+}
+
+module.exports = nextConfig
