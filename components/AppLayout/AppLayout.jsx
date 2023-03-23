3885ae61a769fbf94d0a1d09051fc2d6eac55e67
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2023-03-23 08:50:24 -0400

    3/23/23

--- components/AppLayout/AppLayout.jsx
+++ components/AppLayout/AppLayout.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import {useUser} from "@auth0/nextjs-auth0/client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoins } from "@fortawesome/free-solid-svg-icons";
-import Logo from "../Logo/Logo"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCoins} from "@fortawesome/free-solid-svg-icons";
+import Logo from "../Logo/Logo";
+import PostsContext from "../../context/postsContext";
 
-export default function AppLayout({children, availableTokens, posts, postId}) {
+export default function AppLayout({children, availableTokens, posts: postsFromSSR, postId}) {
   const {user} = useUser();
+
+  const {setPostsFromSSR, posts} = useContext(PostsContext);
+
+  useEffect(() => {
+    setPostsFromSSR(postsFromSSR);
+  }, [postsFromSSR, setPostsFromSSR]);
+  
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col overflow-hidden text-white">
         <div className="px-2 bg-slate-800">
           <div>
-            <Logo/>
+            <Logo />
           </div>
-          <Link
-            href={`/post/new`}
-            className="btn"
-          >
+          <Link href={`/post/new`} className="btn">
             New Post
           </Link>
           <Link href={`/token-topup`} className="block mt-2 text-center">
@@ -28,8 +33,17 @@ export default function AppLayout({children, availableTokens, posts, postId}) {
         </div>
         <div className="flex-1 px-4 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           {posts.map((post) => (
-              <Link key={post._id} href={`/post/${post._id}`} className={`block px-2 my-1 overflow-hidden rounded-sm cursor-pointer text-ellipsis whitespace-nowrap bg-white/10 border border-transparent py-1 ${postId === post._id ? "bg-white/20 border-white" : ""}`}>{post.topic}</Link>
-            ))}
+            <Link
+              key={post._id}
+              href={`/post/${post._id}`}
+              className={`block px-2 my-1 overflow-hidden rounded-sm cursor-pointer text-ellipsis whitespace-nowrap bg-white/10 border border-transparent py-1 ${
+                postId === post._id ? "bg-white/20 border-white" : ""
+              }`}
+            >
+              {post.topic}
+            </Link>
+          ))}
+          <div className="mt-4 text-sm text-center cursor-pointer hover:underline text-slate-400">Load more posts</div>
         </div>
         <div className="flex items-center h-20 gap-2 px-2 border-t bg-cyan-800 border-t-black/50">
           {!!user ? (
--- pages/post/new.jsx
+++ pages/post/new.jsx
@@ -2,58 +2,82 @@ import {withPageAuthRequired} from "@auth0/nextjs-auth0";
 import AppLayout from "../../components/AppLayout/AppLayout";
 import {useState} from "react";
 import {useRouter} from "next/router";
-import { getAppProps } from "../../utils/getAppProps";
+import {getAppProps} from "../../utils/getAppProps";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBrain} from "@fortawesome/free-solid-svg-icons";
 
 export default function NewPost(props) {
   const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [generating, setGenerating] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`/api/generatePost`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({topic, keywords}),
-    });
+    setGenerating(true);
+    try {
+      const response = await fetch(`/api/generatePost`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({topic, keywords}),
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    console.log(json);
-    if (json?.postId) {
-      router.push(`/post/${json.postId}`);
+      console.log(json);
+      if (json?.postId) {
+        router.push(`/post/${json.postId}`);
+      }
+    } catch (e) {
+      setGenerating(false);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <strong>Generate a blog post on the topic of:</strong>
-          </label>
-          <textarea
-            className="block w-full px-4 py-2 my-2 rounded-sm resize-none border-slate-500"
-            value={topic}
-            onChange={(e) => setTopic(e.target.value)}
-          ></textarea>
+    <div className="h-full overflow-hidden">
+      {!!generating && (
+        <div className="flex flex-col items-center justify-center w-full h-full text-green-500 animate-pulse">
+          <FontAwesomeIcon className="text-8xl" icon={faBrain} />
+          <h6>Generating...</h6>
         </div>
-        <div>
-          <label>
-            <strong>Targeting the following keywords:</strong>
-          </label>
-          <textarea
-            className="block w-full px-4 py-2 my-2 rounded-sm resize-none border-slate-500"
-            value={keywords}
-            onChange={(e) => setKeywords(e.target.value)}
-          ></textarea>
+      )}
+      {!generating && (
+        <div className="flex flex-col w-full h-full overflow-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="w-full max-w-screen-sm p-4 m-auto border rounded-md shadow-xl bg-slate-100 border-slate-200 shadow-slate-200"
+          >
+            <div>
+              <label>
+                <strong>Generate a blog post on the topic of:</strong>
+              </label>
+              <textarea
+                className="block w-full px-4 py-2 my-2 rounded-sm resize-none border-slate-500"
+                value={topic}
+                maxLength={80}
+                onChange={(e) => setTopic(e.target.value)}
+              ></textarea>
+            </div>
+            <div>
+              <label>
+                <strong>Targeting the following keywords:</strong>
+              </label>
+              <textarea
+                className="block w-full px-4 py-2 my-2 rounded-sm resize-none border-slate-500"
+                value={keywords}
+                maxLength={80}
+                onChange={(e) => setKeywords(e.target.value)}
+              ></textarea>
+              <small className="block mb-2">Seperate keywords with a comma</small>
+            </div>
+            <button type="submit" className={`btn`} disabled={!topic.trim() || !keywords.trim()}>
+              Generate
+            </button>
+          </form>
         </div>
-        <button type="submit" className={`btn`}>
-          Generate
-        </button>
-      </form>
+      )}
     </div>
   );
 }
@@ -65,9 +89,17 @@ NewPost.getLayout = function getLayout(page, pageProps) {
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(ctx) {
     const props = await getAppProps(ctx);
+
+    if(!props.availableTokens) {
+      return {
+        redirect: {
+          destination: "/token-topup",
+          permanent: false
+        }
+      }
+    }
     return {
       props,
     };
   },
 });
-
